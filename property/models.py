ce9b91810c48320ab97a65ea0e39fc326cb34365
a1exuus <162327737+a1exuus@users.noreply.github.com>
2025-04-09 16:56:19 +0300

    Add model field Flat.new_building

--- property/models.py
+++ property/models.py
@@ -39,13 +39,14 @@ class Flat(models.Model):
         blank=True,
         db_index=True)
 
-    has_balcony = models.NullBooleanField('Наличие балкона', db_index=True)
+    has_balcony = models.BooleanField('Наличие балкона', db_index=True)
     active = models.BooleanField('Активно-ли объявление', db_index=True)
     construction_year = models.IntegerField(
         'Год постройки здания',
         null=True,
         blank=True,
         db_index=True)
+    new_building = models.BooleanField(null=True)
 
     def __str__(self):
         return f'{self.town}, {self.address} ({self.price}р.)'
