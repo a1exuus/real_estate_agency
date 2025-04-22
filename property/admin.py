69690d6408a588f09b5ad6e17fb4a31df45afb72
Alexey <alexexmypride@gmail.com>
2025-04-22 17:34:07 +0300

    Normalize Flat.phonenumbers into Flat.owner_pure_phone

--- property/admin.py
+++ property/admin.py
@@ -7,7 +7,7 @@ from .models import Flat, Report
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ['town', 'address', 'owner']
     readonly_fields = ['created_at']
-    list_display = ['address', 'price', 'construction_year', 'new_building', 'town']
+    list_display = ['address', 'price', 'construction_year', 'new_building', 'town', 'owners_phonenumber', 'owner_pure_number']
     list_editable = ['new_building']
     list_filter = ['new_building', 'rooms_number', 'has_balcony']
     raw_id_fields = ('liked_by',)
---
+++ property/migrations/0016_auto_20250422_1716.py
@@ -0,0 +1,28 @@
+from django.db import migrations
+import phonenumbers
+
+
+def normalize_phone_numbers(apps, schema_editor):
+    Flat = apps.get_model('property', 'Flat')
+    flats = Flat.objects.filter(owners_phonenumber='+70000000000')
+
+    for flat in flats:
+        try:
+            parsed_number = phonenumbers.parse(flat.owners_phonenumber, 'RU')
+            if phonenumbers.is_valid_number(parsed_number):
+                flat.owner_pure_number = parsed_number
+                flat.save()
+        except phonenumbers.NumberParseException:
+            continue
+
+
+class Migration(migrations.Migration):
+    atomic = False
+
+    dependencies = [
+        ('property', '0015_flat_owner_pure_number_alter_flat_owners_phonenumber'),
+    ]
+
+    operations = [
+        migrations.RunPython(normalize_phone_numbers)
+    ]
