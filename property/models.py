296d407b304b06974d20cc90ca231fdcfdd52562
Alexey <alexexmypride@gmail.com>
2025-04-26 17:53:18 +0300

    added `db_index` to `new_building` field

--- property/models.py
+++ property/models.py
@@ -47,7 +47,7 @@ class Flat(models.Model):
         blank=True,
         db_index=True)
     liked_by = models.ManyToManyField(User, verbose_name='Кто лайкнул', blank=True)
-    new_building = models.BooleanField(null=True)
+    new_building = models.BooleanField(null=True, db_index=True)
 
     def __str__(self):
         return f'{self.town}, {self.address} ({self.price}р.)'
