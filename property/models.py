a8d6a1a1acf163fbaac812d0d0803e5873381d87
Alexey <alexexmypride@gmail.com>
2025-05-01 18:00:47 +0300

    dryed `related_name`s in the `Report` model

--- property/models.py
+++ property/models.py
@@ -62,12 +62,12 @@ class Report(models.Model):
         User,
         verbose_name='Кто жаловался',
         on_delete=models.CASCADE,
-        related_name='reporter')
+        related_name='user')
     reported_flat = models.ForeignKey(
         Flat,
         verbose_name='Кваритра, на которую пожаловались',
         on_delete=models.CASCADE,
-        related_name='reported_flat')
+        related_name='flat')
     report_text = models.TextField(blank=True, verbose_name='Описание')
 
     def __str__(self):
